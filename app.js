da942bba5affba992ac010469def507d4930034a
robera16 <roberaworknehrobwork@gmail.com>
2021-10-28 22:30:33 +0300

    added all modules

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const crypto = require("crypto");
+const multer = require("multer");
+const { GridFsStorage } = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const methodOverride = require("method-override");
+const { resolve } = require("path");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
+
+const app = express();
+
+// Passport config
+require("./config/passport")(passport);
+
+// DB Config
+const db = require("./config/keys").MongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+// Ejs
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+// Bodyparser - to access request data
+// extended: fasle -> parsing the url encoded data with querystring library
+// extended: true -> parsing the url encoded data with qs library
+// querystring can't parse a nested object but qs library can,but same interms of purpose
+app.use(express.urlencoded({ extended: false }));
+
+// Express session
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global Vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+// Routes
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server listening on port ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    "mongodb+srv://bradt1234:bradt1234@cluster0.bgtnz.mongodb.net/SEFITA?retryWrites=true&w=majority",
+};
